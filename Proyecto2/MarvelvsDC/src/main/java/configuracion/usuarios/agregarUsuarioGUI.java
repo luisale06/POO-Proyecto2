4590f1c3647ed496cd9942a6605ba0199c9d3440
Franco Rojas <fvrl.cr@gmail.com>
2023-01-19 10:02:28 -0600

    Merge branch 'main' of /Users/franco/Documents/Programación/POO/POO-Proyecto2 with conflicts.

--- Proyecto2/MarvelvsDC/src/main/java/configuracion/usuarios/agregarUsuarioGUI.java
+++ Proyecto2/MarvelvsDC/src/main/java/configuracion/usuarios/agregarUsuarioGUI.java
@@ -1,4 +1,26 @@
 package configuracion.usuarios;
 
+import javax.swing.*;
+
 public class agregarUsuarioGUI {
+    JFrame frame = new JFrame();
+
+    JLabel lblNombre = new JLabel("Nombre:");
+    JLabel lblApellidos = new JLabel("Apellidos");
+    JLabel lblNombreUsuario = new JLabel("Nombre de Usuario:");
+    JLabel lblCorreo = new JLabel("Correo:");
+    JLabel lblContraseña = new JLabel("Contraseña:");
+
+    JTextField txtNombre = new JTextField();
+    JTextField txtApellidos = new JTextField();
+    JTextField txtNombreUsuario = new JTextField();
+    JTextField txtCorreo = new JTextField();
+    JTextField txtContraseña = new JTextField();
+
+    JButton btnAgregarUsuario = new JButton();
+    JButton btnRegresar = new JButton();
+
+    BDusuario usuario = new BDusuario();
+
+    public agregarUsuarioGUI(){}
 }
--- Proyecto2/MarvelvsDC/src/main/java/configuracion/usuarios/usuariosGUI.java
+++ Proyecto2/MarvelvsDC/src/main/java/configuracion/usuarios/usuariosGUI.java
@@ -1,7 +1,7 @@
 package configuracion.usuarios;
 
-import configuracion.ciudades.ciudadesGUI;
-import configuracion.personajes.personajesGUI;
+import configuracion.configuracionGUI;
+
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +13,7 @@ public class usuariosGUI {
     JButton btnAgregarUsuario = new JButton("AGREGAR USUARIO");
     JButton btnEliminarUsuario = new JButton("ELIMINAR USUARIO");
     JButton btnEditarUsuario = new JButton("EDITAR USUARIO");
+    JButton btnRegresar = new JButton("REGRESAR");
 
     public usuariosGUI(){
         frame.setSize(550, 600);
@@ -25,24 +26,30 @@ public class usuariosGUI {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        btnAgregarUsuario.setBounds(120, 150, 295, 45);
+        btnAgregarUsuario.setBounds(120, 120, 295, 45);
         btnAgregarUsuario.setBackground(Color.lightGray);
         btnAgregarUsuario.setForeground(Color.darkGray);
         btnAgregarUsuario.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
 
-        btnEliminarUsuario.setBounds(120, 195+45, 295, 45);
+        btnEliminarUsuario.setBounds(120, 165+45, 295, 45);
         btnEliminarUsuario.setBackground(Color.lightGray);
         btnEliminarUsuario.setForeground(Color.darkGray);
         btnEliminarUsuario.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
 
-        btnEditarUsuario.setBounds(120, 240+90, 295, 45);
+        btnEditarUsuario.setBounds(120, 210+90, 295, 45);
         btnEditarUsuario.setBackground(Color.lightGray);
         btnEditarUsuario.setForeground(Color.darkGray);
         btnEditarUsuario.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
 
+        btnRegresar.setBounds(120, 255+135, 295, 45);
+        btnRegresar.setBackground(Color.lightGray);
+        btnRegresar.setForeground(Color.darkGray);
+        btnRegresar.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
+
         frame.add(btnAgregarUsuario);
         frame.add(btnEliminarUsuario);
         frame.add(btnEditarUsuario);
+        frame.add(btnRegresar);
 
         btnAgregarUsuario.addActionListener(new ActionListener() {
             @Override
@@ -64,6 +71,14 @@ public class usuariosGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 editarUsuarioGUI ventanaEditarUsuario = new editarUsuarioGUI();
+                frame.dispose();
+            }
+        });
+
+        btnRegresar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                configuracionGUI ventanaMenu = new configuracionGUI();
                 frame.dispose();
             }
         });
